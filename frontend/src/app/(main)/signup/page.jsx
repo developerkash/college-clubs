61835dbec59d5182d1513662de20ec98568d09c8
Your Name <2016guptaakash@gmail.com>
2024-06-22 11:45:24 +0530

    formik and yup

--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -61,14 +61,7 @@ const Signup = () => {
       <div className="py-3 flex items-center text-xs text-gray-400 uppercase before:flex-1 before:border-t before:border-gray-200 before:me-6 after:flex-1 after:border-t after:border-gray-200 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
         Or
       </div>
-      <Formik 
-        initialValues={{email: '', password: '', confirmPassword: ''}}
-        onSubmit={loginsubmit}
-      >
-        {
-          ({values, handleChange, handleBlur, handleSubmit}) => (
             
-            // Form
             <form>
               <div className="grid gap-y-4">
                 {/* Form Group */}
