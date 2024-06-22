195669dc7b677dd3411f8c71b9243a2103723bc6
Your Name <2016guptaakash@gmail.com>
2024-06-22 11:45:48 +0530

    formik and yup

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -5,6 +5,14 @@ const Home = () => {
   return (
     <>
       <Navbar />
+
+      <section className="w-full">
+  <div className="w-full h-[520px] bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center ">
+    {/* Photo by '@insolitus' on Unsplash */}
+    
+  </div>
+</section>
+
       
     </>
   )
