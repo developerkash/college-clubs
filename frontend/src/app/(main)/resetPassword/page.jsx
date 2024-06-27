f5f74ef70e36cd24e7ad84dffec35262896d71a2
Your Name <2016guptaakash@gmail.com>
2024-06-27 12:43:42 +0530

    commit

--- frontend/src/app/(main)/resetPassword/page.jsx
+++ frontend/src/app/(main)/resetPassword/page.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const ResetPassword = () => {
   return (
-    <div>ResetPassword</div>
-  )
-}
+    <div
+      className="min-h-screen  bg-red-100 text-gray-900 flex justify-center"
+      style={{ backgroundColor: "#9A616D" }}
+    >
+    </div>
+  );
+};
 
 export default ResetPassword;
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+'use client';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import Navbar from './(main)/navbar'
 
 const Home = () => {
   return (
     <>
       <Navbar />
-
-      <section className="w-full">
-  <div className="w-full h-[520px] bg-[url('https://images.unsplash.com/photo-1449844908441-8829872d2607?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxob21lfGVufDB8MHx8fDE3MTA0MDE1NDZ8MA&ixlib=rb-4.0.3&q=80&w=1080')] bg-cover bg-no-repeat bg-center flex flex-col justify-center items-center ">
-    {/* Photo by '@insolitus' on Unsplash */}
-    
-  </div>
-</section>
-
-      
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className=''>
+          <img src="https://thumbs.dreamstime.com/z/kolesnikov-evgeny-21650495.jpg?ct=jpeg" alt=""  />
+        </SwiperSlide>
+        <SwiperSlide>
+        Slide 2
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        
+      </Swiper>
     </>
   )
 }
