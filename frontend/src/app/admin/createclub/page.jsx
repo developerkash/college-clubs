7c81ceaf47f6b68f95ba0bda087ffae59f6df0ec
Your Name <2016guptaakash@gmail.com>
2024-07-12 12:24:05 +0530

    ...

--- frontend/src/app/admin/createclub/page.jsx
+++ frontend/src/app/admin/createclub/page.jsx
@@ -33,20 +33,20 @@ const CreateClub = () => {
       }
     },
 
-    const uploadFile = (e) => {
-      const file = e.target.files[0];
-      const fd = new FormData();
-      fd.append("myfile", file);
-      fetch("http://localhost:5000/utils/uploadfile", {
-        method: "POST",
-        body: fd,
-      }).then((res) => {
-        if (res.status == 200) {
-          console.log("File Uploaded Successfully");
-          createnewclub.values.image = file.name;
-        } 
-      });
-    }
+    // const uploadFile = (e) => {
+    //   const file = e.target.files[0];
+    //   const fd = new FormData();
+    //   fd.append("myfile", file);
+    //   fetch("http://localhost:5000/utils/uploadfile", {
+    //     method: "POST",
+    //     body: fd,
+    //   }).then((res) => {
+    //     if (res.status == 200) {
+    //       console.log("File Uploaded Successfully");
+    //       createnewclub.values.image = file.name;
+    //     } 
+    //   });
+    // }
         
       
 
